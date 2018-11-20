ede6a2f5a8a04283baba7685df0c97794782cc6e
Wojciech Bury <bury1992@gmail.com>
2018-11-20 16:04:33 +0100

    Final commit

---
+++ src/main/java/pl/wojciechbury/organiser/models/LoginInterceptor.java
@@ -0,0 +1,41 @@
+package pl.wojciechbury.organiser.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Configuration
+public class LoginInterceptor extends HandlerInterceptorAdapter {
+
+    final UserSession userSession;
+    public static final String PASSWORD_HEADER_API = "api-password";
+
+    @Value("${api}")
+    String ourApiKey;
+
+    @Autowired
+    public LoginInterceptor(UserSession userSession){
+        this.userSession = userSession;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(request.getRequestURI().contains("register") || request.getRequestURI().contains("login") || userSession.isLoggedIn()){
+            return true;
+        }
+        if(request.getRequestURI().contains("api")){
+            String passwordApi = request.getHeader(PASSWORD_HEADER_API);
+
+            if(passwordApi == null || !passwordApi.equals(ourApiKey)){
+                response.sendError(403, "Bad api key");
+                return false;
+            }
+        }
+        return super.preHandle(request, response, handler);
+    }
+}
---
+++ src/main/java/pl/wojciechbury/organiser/models/WebConfig.java
@@ -0,0 +1,25 @@
+package pl.wojciechbury.organiser.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    final LoginInterceptor loginInterceptor;
+
+    @Autowired
+    public WebConfig(LoginInterceptor loginInterceptor){
+        this.loginInterceptor = loginInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor);
+    }
+
+
+}
